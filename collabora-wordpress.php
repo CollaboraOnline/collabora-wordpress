152d442bd9c95de448e4a0f4e613b8d4cecd918a
Hubert Figuière <hub@collabora.com>
2024-09-26 09:30:46 -0400

    Initial bootstrap
    Signed-off-by: Hubert Figuière <hub@collabora.com>

---
+++ collabora-wordpress.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * @package           collabora-wordpres
+ * @author            Collabora Productivity
+ * @copyright         2024 Collabora Productivity
+ * @license           MPL-2.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: Collabora Online for Wordpress
+ * Plugin URI: https://github.com/hfiguiere/collabora-wordpress
+ * Description: Wordpress connector for Collabora Online
+ * Version: 0.1.0
+ * License: MPL-2.0
+ * License URI: https://www.mozilla.org/en-US/MPL/2.0/
+ *
+ * Spdx-License: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+?>
