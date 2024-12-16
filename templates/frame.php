8127e7b999782bb85b306282b3109c92f985292b
Hubert Figuière <hub@collabora.com>
2024-12-16 15:32:12 -0500

    frontend: the assets directory is used for something else
    Signed-off-by: Hubert Figuière <hub@collabora.com>

--- templates/frame.php
+++ templates/frame.php
@@ -29,8 +29,8 @@ $args = wp_parse_args(
 	<meta charset="UTF-8" />
 <?php // phpcs:disable WordPress.WP.EnqueuedResources.NonEnqueuedScript ?>
 <?php // phpcs:disable WordPress.WP.EnqueuedResources.NonEnqueuedStylesheet ?>
-	<link rel="stylesheet" id="collabora-wordpress-cool-css" href="<?php echo esc_url( $args['base_url'] ); ?>/assets/css/cool.css" media="all">
-	<script id="collabora-wordpress-cool-js" src="<?php echo esc_url( $args['base_url'] ); ?>/assets/js/cool.js"></script>
+	<link rel="stylesheet" id="collabora-wordpress-cool-css" href="<?php echo esc_url( $args['base_url'] ); ?>/public/css/cool.css" media="all">
+	<script id="collabora-wordpress-cool-js" src="<?php echo esc_url( $args['base_url'] ); ?>/public/js/cool.js"></script>
 <?php // phpcs:enable WordPress.WP.EnqueuedResources.NonEnqueuedStylesheet ?>
 <?php // phpcs:enable WordPress.WP.EnqueuedResources.NonEnqueuedScript ?>
 </head>
